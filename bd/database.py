8d1ffbabd23dd23773f0b4dd5bc5f0a49ae79778
Carlos <jcarlosgarcial@outlook.com>
2024-06-13 21:18:40 -0400

    Update database.py

--- bd/database.py
+++ bd/database.py
@@ -3,10 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#sqliteName = '../movies.sqlite'
-#base_dir = os.path.dirname(os.path.realpath(__file__))
-#datebaseUrl = f"sqlite:///{os.path.join(base_dir, sqliteName)}"
-
 #RUTA DE LA BD
 DATABASE_URL = "sqlite:///movies.sqlite"
 
