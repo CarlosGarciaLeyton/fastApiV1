c118a6bf3db238b97cc012a1809462d783a0c212
jcarlosgarcial <jcarlosgarcial@outlook.com>
2024-06-14 19:54:29 -0400

    delete archivo punto.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
-from pydantic import BaseModel
-from bd.database import engine, Base
-from routers.moviessss import routerMovie
-from routers.users import login_user
+from fastapi import FastAPI
 import uvicorn
-import os
+
+import models.movie as model
+from bd.database import engine
+from routers.routes_movie import router as router_crud
+from routers.users import login_user
+
+model.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
-    title='api facil',
-    description='api de prueba con sqlite',
-    version='0.0.1'
+    title="API FACIL",
+    description="uso de crud con apis",
+    version="1.0.0"
 )
 
-app.include_router(login_user)
-app.include_router(routerMovie)
+
+'''@app.get("/")
+def hello_world():
+    return {
+        "msg": "Hola Mundo"
+    }'''
 
 
-Base.metadata.create_all(bind=engine)
+app.include_router(router=login_user, tags=["LOGIN"], prefix="/movies")
+app.include_router(router=router_crud, tags=["CRUD"], prefix="/movies")
 
 
-'''if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run ("main:app", host="127.0.0.1", port=port)'''
-
+if __name__ == "__main__":
+    uvicorn.run("main:app",
+                host="localhost",
+                reload=True)
